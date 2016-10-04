635676e959dd7138551928b653b3a687b973b524
Ian Lantzy <ilantzy@udel.edu>
2016-10-04 21:15:06 +0000

    b_inst working

--- instruction/b_inst.py
+++ instruction/b_inst.py
@@ -14,21 +14,27 @@ class b_inst:
     opcode = 0b0
 
     operation = None
+    op_args = ["proc", "BR_address"]
 
-    def execute(BR_address):
-        if type(BR_address) not int:
+
+    def execute(self, proc, BR_address):
+        if not isinstance(proc, core):
+            raise TypeError("proc must be an ARMv8Core")
+        if type(BR_address) is not int:
             raise TypeError("BR_address must be of type int")
-        else if BR_address < 0 or BR_address > pow(2, 26):
+        elif BR_address < 0 or BR_address > pow(2, 26):
             raise ValueError("BR_address value out of range for 26 bits")
-        if operation:
-            operation(BR_address)
+        if self.operation is not None:
+            self.operation(proc, BR_address)
 
     def __init__(self, opcode, operation):
-        if type(opcode) not int:
+        if type(opcode) is not int:
             raise TypeError("opcode must be of type int")
         args = getargspec(operation).args
-        if len(args) > 1 or args[0] != "BR_address":
-            raise TypeError("operation must be a function accepting 1 " +
-                            "argument: BR_address")
+        if len(args) > len(self.op_args) or args != self.op_args:
+            err_str = ("operation must be a function " +
+                       "accepting {} ".format(len(self.op_args)) +
+                       "arguments: {}".format(self.op_args))
+            raise TypeError(err_str)
         self.opcode = opcode
         self.operation = operation
