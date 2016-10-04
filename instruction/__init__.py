a20938b9dcc9d47fd373525077ccd2d021a30f49
Ian Lantzy <ilantzy@udel.edu>
2016-10-04 18:51:00 +0000

    Added imports for each instruction type file

--- instruction/__init__.py
+++ instruction/__init__.py
@@ -2,3 +2,9 @@
 This module provides all implemented ARMv8 instructions to be simulated.
 """
 from armv8_core import ARMv8Core as core
+import r_inst
+import i_inst
+import d_inst
+import b_inst
+import cb_inst
+import iw_inst
