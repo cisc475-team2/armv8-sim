169e0000a18ff13ed8a17911e33a397717c2005f
Ian Lantzy <ilantzy@udel.edu>
2016-10-04 21:35:21 +0000

    cb_inst working

--- instruction/cb_inst.py
+++ instruction/cb_inst.py
@@ -14,3 +14,27 @@ class cb_inst:
     opcode = 0b0
 
     operation = None
+    op_args = ["proc","COND_BR_address","Rt"]
+    
+
+    def execute(self, proc, COND_BR_address, Rt):
+        if not isinstance(proc, core):
+            raise TypeError("proc must be an ARMv8Core")
+        if type(COND_BR_address) is not int:
+            raise TypeError("COND_BR_address must be of type int")
+        if type(Rt) is not int:
+            raise TypeError("Rt must be of type int")
+        if self.operation is not None:
+            self.operation(proc, COND_BR_address, Rt)
+
+    def __init__(self, opcode, operation):
+        if type(opcode) is not int:
+            raise TypeError("opcode must be of type int")
+        args = getargspec(operation).args
+        if len(args) > len(self.op_args) or args != self.op_args:
+            err_str = ("operation must be a function " +
+                       "accepting {} ".format(len(self.op_args)) +
+                       "arguments: {}".format(self.op_args))
+            raise TypeError(err_str)
+        self.opcode = opcode
+        self.operation = operation
