dcdc2b22d1c7cb33d41cfd2420ff1117e215fa0f
Ian Lantzy <ilantzy@udel.edu>
2016-10-04 21:31:21 +0000

    d_inst working

--- instruction/d_inst.py
+++ instruction/d_inst.py
@@ -15,22 +15,30 @@ class d_inst:
     opcode = 0b0
 
     operation = None
+    op_args = ["proc","T_address","op","Rn","Rt"]
 
-    def execute(DT_address, op, Rn, Rt):
-        if operation:
-            operation(DT_address, op, Rn, Rt)
+    def execute(self, proc, T_address, op, Rn, Rt):
+        if not isinstance(proc, core):
+            raise TypeError("proc must be an ARMv8Core")
+        if type(T_address) is not int:
+            raise TypeError("T_address must be of type int")
+        if type(op) is not int:
+            raise TypeError("op must be of type int")
+        if type(Rn) is not int:
+            raise TypeError("Rn must be of type int")
+        if type(Rt) is not int:
+            raise TypeError("Rt must be of type int")
+        if self.operation is not None:
+            self.operation(proc, T_address, op, Rn, Rt)
 
-    def __init__(self, name, opcode, operation):
-        if type(name) not str:
-            raise TypeError("name must be of type str")
-        if type(opcode) not int:
+    def __init__(self, opcode, operation):
+        if type(opcode) is not int:
             raise TypeError("opcode must be of type int")
         args = getargspec(operation).args
-        a = set(args)
-        b = set(["DT_address", "op", "Rn", "Rt"])
-        if len(args) != 4 or a == b:
-            raise TypeError("operation must be a function accepting 4 " +
-                            "arguments: DT_address, op, Rn, Rt")
-        self.name = name
+        if len(args) > len(self.op_args) or args != self.op_args:
+            err_str = ("operation must be a function " +
+                       "accepting {} ".format(len(self.op_args)) +
+                       "arguments: {}".format(self.op_args))
+            raise TypeError(err_str)
         self.opcode = opcode
         self.operation = operation
