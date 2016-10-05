151dd118a5b7b0f3e8de11a24af1b330a2d5ec01
Ian Lantzy <ilantzy@udel.edu>
2016-10-05 05:26:14 +0000

    i_inst, iw_inst, and r_inst working

--- instruction/iw_inst.py
+++ instruction/iw_inst.py
@@ -14,3 +14,28 @@ class iw_inst:
     opcode = 0b0
 
     operation = None
+    op_args = ["proc","MOV_immediate","Rd"]
+            
+
+    def execute(self, proc, MOV_immediate, Rd):
+        if not isinstance(proc, core):
+            raise TypeError("proc must be an ARMv8Core")
+        if type(MOV_immediate) is not int:
+            raise TypeError("MOV_immediate must be of type int")
+        if type(Rd) is not int:
+            raise TypeError("Rd must be of type int")
+        if self.operation is not None:
+            self.operation(proc, MOV_immediate, Rd)
+
+
+    def __init__(self, opcode, operation):
+        if type(opcode) is not int:
+            raise TypeError("opcode must be of type int")
+        args = getargspec(operation).args
+        if len(args) > len(self.op_args) or args != self.op_args:
+            err_str = ("operation must be a function " +
+                       "accepting {} ".format(len(self.op_args)) +
+                       "arguments: {}".format(self.op_args))
+            raise TypeError(err_str)
+        self.opcode = opcode
+        self.operation = operation
--- instruction/r_inst.py
+++ instruction/r_inst.py
@@ -14,3 +14,32 @@ class r_inst:
     opcode = 0b0
 
     operation = None
+    op_args = ["proc","Rm","shamt","Rn","Rd"]
+                
+
+    def execute(self, proc, Rm, shamt, Rn, Rd):
+        if not isinstance(proc, core):
+            raise TypeError("proc must be an ARMv8Core")
+        if type(Rm) is not int:
+            raise TypeError("Rm must be of type int")
+        if type(shamt) is not int:
+            raise TypeError("shamt must be of type int")
+        if type(Rn) is not int:
+            raise TypeError("Rn must be of type int")
+        if type(Rd) is not int:
+            raise TypeError("Rd must be of type int")
+        if self.operation is not None:
+            self.operation(proc, Rm, shamt, Rn, Rd)
+
+
+    def __init__(self, opcode, operation):
+        if type(opcode) is not int:
+            raise TypeError("opcode must be of type int")
+        args = getargspec(operation).args
+        if len(args) > len(self.op_args) or args != self.op_args:
+            err_str = ("operation must be a function " +
+                       "accepting {} ".format(len(self.op_args)) +
+                       "arguments: {}".format(self.op_args))
+            raise TypeError(err_str)
+        self.opcode = opcode
+        self.operation = operation
