b2658ddabac9b43e663a7f222b15257355463de0
Ian Lantzy <ilantzy@udel.edu>
2016-10-11 22:49:00 +0000

    Registers should be referenced by string

--- instruction/r_inst.py
+++ instruction/r_inst.py
@@ -33,13 +33,13 @@ class r_inst:
     def execute(self, proc, Rm, shamt, Rn, Rd):
         if not isinstance(proc, core):
             raise TypeError("proc must be an ARMv8Core")
-        if type(Rm) is not int:
+        if type(Rm) is not str:
             raise TypeError("Rm must be of type int")
         if type(shamt) is not int:
             raise TypeError("shamt must be of type int")
-        if type(Rn) is not int:
+        if type(Rn) is not str:
             raise TypeError("Rn must be of type int")
-        if type(Rd) is not int:
+        if type(Rd) is not str:
             raise TypeError("Rd must be of type int")
         if self.operation is not None:
             self.operation(proc, Rm, shamt, Rn, Rd)
