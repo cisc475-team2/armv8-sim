48f1f0a3a5eeacac5844f6575318b778957c9a9c
Ian Lantzy <ilantzy@udel.edu>
2016-10-11 22:50:00 +0000

    Added ADD instruction

---
+++ armv8_isa/__init__.py
@@ -0,0 +1,8 @@
+"""
+This package provides the ARMv8 ISA
+"""
+# Import instruction classes
+from instruction import R, I, D, B, CB, IW
+
+# Import instructions from modules below
+from add import ADD
