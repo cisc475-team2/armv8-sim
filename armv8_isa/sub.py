d656eb2abba112aeee8789869e5e3a58b77cafbf
deanarno <dcarnold@udel.edu>
2016-11-15 18:06:52 +0000

    updated SUB function

--- armv8_isa/sub.py
+++ armv8_isa/sub.py
@@ -5,10 +5,11 @@ from armv8_isa import R
 
 def operation(proc, Rm, shamt, Rn, Rd):
     raw = proc.reg[Rn].get() - proc.reg[Rm].get()
-    if raw < proc.reg[Rd].data_min():
-        result = 0
-    else:
-        result = raw
+    result = int(raw % proc.reg[Rd].data_max())
+    #if raw < proc.reg[Rd].data_min():
+    #    result = 0
+    #else:
+    #    result = raw
     proc.reg[Rd].set(result)
 
 SUB = R(0x658, operation)
