6bb54d8936683a333e106e540c79f16b160e42d7
Ian Lantzy <ilantzy@udel.edu>
2016-12-07 22:23:03 +0000

    Partially working file parsing for R instructions

--- armv8_file.py
+++ armv8_file.py
@@ -2,6 +2,7 @@
 This module handles file parsing for armv8-sim.
 """
 from armv8_core import ARMv8Core
+import armv8_isa
 
 
 def coreParseFile(core, file):
@@ -23,10 +24,34 @@ def coreParseFile(core, file):
     
     PC = 0
     for line in lines:
-        cline = [x.replace(',','') for x in line.split()]
+        cline = [x.replace(',','') for x in line.lstrip().split()]
         code[PC] = cline
     
         PC = PC + 4
+    
+    while core.reg["PC"].get() < PC:
+        corePC = core.reg["PC"].get()
+        if code[corePC][0] in armv8_isa.instset:
+            i = armv8_isa.instset[code[corePC][0]]
+            if i.inst_format == "B":
+                print "B"
+            elif i.inst_format == "CB":
+                print "CB"
+            elif i.inst_format == "D":
+                print "D"
+            elif i.inst_format == "I":
+                print "I"
+            elif i.inst_format == "IW":
+                print "IW"
+            elif i.inst_format == "R":
+                print "R"
+                i.execute(core,
+                          code[corePC][3],
+                          0,
+                          code[corePC][2],
+                          code[corePC][1])
+        
+        core.reg["PC"].set(corePC + 4)
     
     print labels
     print code
